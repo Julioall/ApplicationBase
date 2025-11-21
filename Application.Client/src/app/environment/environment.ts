9709692a5b18e18691981d4da3578d7ac98ee61f
Julio Alves <julio.all@outlook.com>
2025-11-21 19:12:51 -0300

    chore: configurar fileReplacements para usar environment de produção

--- Application.Client/src/app/environment/environment.ts
+++ Application.Client/src/app/environment/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:5095/api',
-    googleClientId: '1006899498219-l4p4rkd1lsk5p9714np43hd5lq11ke87.apps.googleusercontent.com',
-    facebookAppId: 'YOUR_FACEBOOK_APP_ID'
   };
